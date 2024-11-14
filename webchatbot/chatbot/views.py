b9af5eeca3fcd1ab4db80edaea7a657eb93a980c
Danish khan <danishyazdani063@gmail.com>
2024-11-14 13:44:47 +0500

    first commit

---
+++ webchatbot/chatbot/views.py
@@ -0,0 +1,5 @@
+# chatbot/views.py
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'chatbot/home.html')
---
+++ webchatbot/webchatbot/urls.py
@@ -0,0 +1,8 @@
+# webchat/urls.py
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('chatbot.urls')),  # Include chatbot app URLs
+]
